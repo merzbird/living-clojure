c990d394afaf033e0c97181d420badbeb85a457e
Yurii Melnychuk <yu.melnychuk@gmail.com>
2019-09-29 20:38:31 +0300

    living clojure: webapp step 2

--- project.clj
+++ project.clj
@@ -2,11 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-json "0.5.0"]
-                 [org.clojure/clojurescript "1.10.520"]]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [cljs-http "0.1.46"]
+                 [org.clojure/core.async "0.4.500"]
+                 [enfocus "2.1.0"]]
   :plugins [
             [lein-ring "0.12.5"]
             [lein-cljsbuild "1.1.7"]]
--- src-cljs/cheshire_cat/core.cljs
+++ src-cljs/cheshire_cat/core.cljs
@@ -1,4 +1,25 @@
-(ns cheshire-cat.core)
+(ns cheshire-cat.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
+            [enfocus.core :as ef]
+            [enfocus.events :as ev]
+            [enfocus.effects :as ee]))
+
+(defn say-goodbye []
+  (ef/at
+    "#cat-name" (ee/fade-out 500)
+    "#button1" (ee/fade-out 1000)
+    "#status" (ee/fade-out 1500)
+    ))
 
 (defn ^:export init []
-      (js/alert "hi"))
+  (go
+    (let [response (<! (http/get "/cheshire-cat"))
+          body (:body response)]
+      (ef/at "#cat-name" (ef/content (:name body)))
+      (ef/at "#status" (ef/do->
+                         (ef/content (:status body))
+                         (ef/set-style :font-size "500%")))
+      (ef/at "#button1" (ev/listen :click say-goodbye))
+      )))
