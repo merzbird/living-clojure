22f0acf55e40dfd47a421bd397e60ec5d9c8dc5c
Yurii Melnychuk <yu.melnychuk@gmail.com>
2019-12-26 18:33:47 +0200

    weekly training: week3 day 1

--- weekly-training/test/weekly/core_test.clj
+++ weekly-training/test/weekly/core_test.clj
@@ -61,8 +61,28 @@
                     [1])))
   )
 
+(defn tree?
+  [c]
+  (if (and (sequential? c) (= 3 (count c)))
+    (let [[n [:as l] [:as r]] c]
+      (if (and (not (nil? n)) (or (nil? l) (tree? l)) (or (nil? r) (tree? r))) true false)
+      )
+    false)
+  )
+
+;; TODO
+(defn symmetric-tree?
+  [c]
+  (if (and (sequential? c) (= 3 (count c)))
+    (let [[n [:as l] [:as r]] c]
+      (println (str "c " c " n " n " l " l " r " r))
+      (if (and (not (nil? n)) (or (nil? l) (tree? l)) (or (nil? r) (tree? r))) true false)
+      )
+    false)
+  )
 (deftest week2
   (testing "exercises"
+    ;; week2
     (is (= (fib 8) '(1 1 2 3 5 8 13 21)))
     (is (= (uppercased "$#A(*&987Zf") "AZ"))
     (is (= 6 (some #{2 7 6} [5 6 7 8])))
@@ -75,6 +95,21 @@
     (is (= (cartesian #{1 2 3} #{4 5}) #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]}))
     (is (= (symmetric-diff #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7}))
     (is (== (lcm 7 5/7 2 3/5) 210))
-    (is (= (map pascal (range 1 6)) [[1] [1 1] [1 2 1] [1 3 3 1] [1 4 6 4 1] ]))
+    (is (= (map pascal (range 1 6)) [[1] [1 1] [1 2 1] [1 3 3 1] [1 4 6 4 1]]))
     (is (= (pascal 11) [1 10 45 120 210 252 210 120 45 10 1]))
+    ;; week3
+    (is (= (tree? '(:a (:b nil nil) nil)) true))
+    (is (= (tree? [1 [2 [3 [4 nil nil] nil] nil] nil]) true))
+    (is (= (tree? [1 nil [2 [3 nil nil] [4 nil nil]]]) true))
+    (is (= (tree? [1 [2 [3 [4 false nil] nil] nil] nil]) false))
+
+    (is (= (symmetric-tree? '(:a (:b nil nil) (:b nil nil))) true))
+    (is (= (symmetric-tree?
+             [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+                [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]])
+           false))
+    (is (= (symmetric-tree?
+             [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+                [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]])
+           true))
     ))
