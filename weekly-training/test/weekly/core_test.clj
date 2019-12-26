e04c67889b1bf8078b9518697e73e347c02061d8
Yurii Melnychuk <yu.melnychuk@gmail.com>
2019-12-26 13:04:46 +0200

    weekly training: week2 day 4 ex 88

--- weekly-training/test/weekly/core_test.clj
+++ weekly-training/test/weekly/core_test.clj
@@ -13,4 +13,5 @@
     (is (= ((fn [x y] (first (filter #(and (= 0 (rem x %)) (= 0 (rem y %))) (range (min x y) 0 -1)))) 1023 858) 33))
     (is (= [1 8 27 64] (map (#(fn [x] (int (Math/pow x %))) 3) [1 2 3 4])))
     (is (= ((fn [c1 c2] (set (mapcat (fn [i] (map (fn [j] [i j]) c2)) c1))) #{1 2 3} #{4 5}) #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]}))
+    (is (= ((fn [set1 set2] (clojure.set/union (clojure.set/difference set1 set2) (clojure.set/difference set2 set1))) #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7}))
     ))
